1e9ad38af0017c4f4677b82c47943c0711116f0a
shinchan2222 <gold777army@gmail.com>
2026-08-21 06:54:58 +0530

    Project cleanup: remove obsolete files, update download link to v19, and optimize SW cache list

--- www/sw.js
+++ www/sw.js
@@ -8,9 +8,6 @@ const ASSETS_TO_CACHE = [
     './manifest.json',
     './src/firebase.js',
     './src/plugins/local-notifications.js',
-    './assets/icons/icon-72x72.png',
-    './assets/icons/icon-192x192.png',
-    './assets/icons/icon-512x512.png',
     'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Outfit:wght@400;500;600;700;800&display=swap',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
 ];
